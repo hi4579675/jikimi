3d6d0b98e8ac5554ed82aed5885265e022c86071
hi4579675 <hi4579675@gmail.com>
2026-04-01 14:06:26 +0900

    feat(backend): add Spring Boot starter scaffold

---
+++ backend/jikimi/src/test/java/com/jikimi/backend/JikimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikimi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
